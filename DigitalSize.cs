0b3b33927cb57afa5ba62f923cab4d1c1a76c4d3
[abdo bakr] <[abakr3912@gmail.com]>
2024-11-05 21:51:29 +0200

    Initial commit of DigitalSize project

---
+++ DigitalSize.cs
@@ -0,0 +1,61 @@
+﻿namespace Struct
+{
+    public class DigitalSize
+    {
+        private long bit;
+
+        public string Bit => $"{(bit / BitsInBit):N0} Bit";
+        public string Byte => $"{(bit / BitsInByte):N0} Byte";
+        public string KB => $"{(bit / BitsInKB):N0} KB";
+        public string MB => $"{(bit / BitsInMB):N0} MB";
+        public string GB => $"{(bit / BitsInGB):N0} GB";
+        public string TB => $"{(bit / BitsInTB):N0} TB";
+
+        private const long BitsInBit = 1;
+        private const long BitsInByte = 8;
+        private const long BitsInKB = BitsInByte * 1024;
+        private const long BitsInMB = BitsInKB * 1024;
+        private const long BitsInGB = BitsInMB * 1024;
+        private const long BitsInTB = BitsInGB * 1024;
+
+        public DigitalSize(long initialValue)
+        {
+            this.bit = initialValue;
+        }
+
+        public DigitalSize AddBit(long bit)
+        {
+            return Add(bit, BitsInBit);
+        }
+
+        public DigitalSize AddByte(long byteValue)
+        {
+            return Add(byteValue, BitsInByte);
+        }
+
+        public DigitalSize AddKB(long kbValue)
+        {
+            return Add(kbValue, BitsInKB);
+        }
+
+        public DigitalSize AddMB(long mbValue)
+        {
+            return Add(mbValue, BitsInMB);
+        }
+
+        public DigitalSize AddGB(long gbValue)
+        {
+            return Add(gbValue, BitsInGB);
+        }
+
+        public DigitalSize AddTB(long tbValue)
+        {
+            return Add(tbValue, BitsInTB);
+        }
+
+        private DigitalSize Add(long value, long scale)
+        {
+            return new DigitalSize(this.bit + value * scale);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalSize size = new DigitalSize(60000000000000000);
+            DigitalSize size1 = size.AddBit(8);
+
+            Console.WriteLine(size.Bit);
+            Console.WriteLine(size.Byte);
+            Console.WriteLine(size.KB);
+            Console.WriteLine(size.MB);
+            Console.WriteLine(size.GB);
+            Console.WriteLine(size.TB);
+
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine(size1.Bit);
+            Console.WriteLine(size1.Byte);
+            Console.WriteLine(size1.KB);
+            Console.WriteLine(size1.MB);
+            Console.WriteLine(size1.GB);
+            Console.WriteLine(size1.TB);
+
+            Console.ReadKey();
+        }
+    }
+}
